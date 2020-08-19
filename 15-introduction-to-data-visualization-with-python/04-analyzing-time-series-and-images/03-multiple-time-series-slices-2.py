4b6ddec1c6e3108563f185d716096766ce243eea
kahhuey <69929494+kahhuey@users.noreply.github.com>
2020-08-20 04:50:05 +0800

    Update 03-multiple-time-series-slices-2.py

--- 15-introduction-to-data-visualization-with-python/04-analyzing-time-series-and-images/03-multiple-time-series-slices-2.py
+++ 15-introduction-to-data-visualization-with-python/04-analyzing-time-series-and-images/03-multiple-time-series-slices-2.py
@@ -12,20 +12,20 @@ Reassign the slice view to contain data from the series aapl for January 2008. T
 Plot the slice view in 'green' in the bottom subplot with the xticks rotated to 45 degrees.
 '''
 # Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
-view = aapl['2007-11':'2008-04']
+view_1 = aapl['2007-11':'2008-04']
 
 # Plot the sliced series in the top subplot in red
 plt.subplot(2,1,1)
-plt.plot(view, color='red')
+plt.plot(view_1, color='red')
 plt.title('AAPL: Nov. 2007 to Apr. 2008')
 plt.xticks(rotation=45)
 
 # Reassign the series by slicing the month January 2008
-view = aapl['2008-01']
+view_2 = aapl['2008-01']
 
 # Plot the sliced series in the bottom subplot in green
 plt.subplot(2,1,2)
-plt.plot(view, color='green')
+plt.plot(view_2, color='green')
 plt.title('AAPL: Jan. 2008')
 plt.xticks(rotation=45)
 
